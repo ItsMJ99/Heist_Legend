7846dfb0636cebf91a8c7fa61809843b943a0608
ItsMJ99 <madhav3jadhav@gmail.com>
2023-07-23 21:21:59 +0530

    minor changes

---
+++ Source/Heist_Legend/Mover.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Mover.h"
+
+// Sets default values for this component's properties
+UMover::UMover()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+// Called when the game starts
+void UMover::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+}
+
+// Called every frame
+void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	float t1 = 10;
+	float *t2 = &t1;
+	UE_LOG(LogTemp, Display, TEXT("t1 value = %f"), t1);		   // 10
+	UE_LOG(LogTemp, Display, TEXT("t1 Address = %u"), &t1);		   // t1 add
+	UE_LOG(LogTemp, Display, TEXT("t1 from t2 Address = %u"), t2); // t1 add
+	UE_LOG(LogTemp, Display, TEXT("t2 Address = %u"), &t2);		   // t2 add
+	UE_LOG(LogTemp, Display, TEXT("t1 value = %f"), *t2);		   // t2 val which is from t1 = 10
+
+	AActor *Owner = GetOwner();
+	// FString name = (*Owner).GetActorNameOrLabel();
+	FString Name = Owner->GetActorNameOrLabel();
+	FString Location = Owner->GetActorLocation().ToCompactString();
+
+	UE_LOG(LogTemp, Display, TEXT("Mover Owner Address = %u"), Owner); // Actor add
+	UE_LOG(LogTemp, Display, TEXT("Actor Name = %s"), *Name);
+	UE_LOG(LogTemp, Display, TEXT("Actor Name = %s"), *Location);
+	// ...
+}
